a5cc70374f91351fbaf39251faf8daecf9e7142f
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-10-22 23:51:27 +0530

    feat: add spring-web, jpa , lombok, mysql with spring starter

---
+++ src/main/java/models/UrlMapping.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrl;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // this specifies foreign key linking to users table
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
+}
